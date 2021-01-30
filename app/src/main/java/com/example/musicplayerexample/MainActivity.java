55ada90f3189fad258a0a7743fc436123b63455f
jiyeong1004 <ak1157@naver.com>
2021-01-30 17:56:48 +0900

    재생버튼 눌렀을때

--- app/src/main/java/com/example/musicplayerexample/MainActivity.java
+++ app/src/main/java/com/example/musicplayerexample/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.musicplayerexample;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_play;
+    Button btn_stop;
+
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_play = findViewById(R.id.btn_play);
+        btn_stop = findViewById(R.id.btn_stop);
+
+        // 재생버튼 눌렀을때
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.music);
+                mediaPlayer.start();
+            }
+        });
+
+        // 정지버튼 눌렀을때
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()){
+
+                }
+            }
+        });
+
     }
 }
